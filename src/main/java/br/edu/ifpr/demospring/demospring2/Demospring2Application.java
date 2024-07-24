eb9d45b89208de0af67c98a135609914eb14f599
IFPR <ifpr@jacarezinho.ifpr>
2024-07-23 21:59:26 -0300

    feat: projeto iniciado

---
+++ src/main/java/br/edu/ifpr/demospring/demospring2/Demospring2Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.demospring.demospring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demospring2Application.class, args);
+    }
+
+}
