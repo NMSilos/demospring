b8c0c931dbe3e321be9c04a2589299404095342c
IFPR <ifpr@jacarezinho.ifpr>
2024-07-24 20:51:50 -0300

    feat: classe Departamento

--- src/main/java/br/edu/ifpr/demospring/demospring2/domain/Departamento.java
+++ src/main/java/br/edu/ifpr/demospring/demospring2/domain/Departamento.java
@@ -1,4 +1,20 @@
 package br.edu.ifpr.demospring.demospring2.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "departamentos")
 public class Departamento extends AbstractEntity <Long> {
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String nome;
+
 }
