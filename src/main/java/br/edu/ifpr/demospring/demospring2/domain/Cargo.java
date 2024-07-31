6ea4c8da7518bd1842825bb093e15a1dba6166b4
IFPR <ifpr@jacarezinho.ifpr>
2024-07-31 20:13:45 -0300

    feat: classe Cargo criada

--- src/main/java/br/edu/ifpr/demospring/demospring2/domain/Cargo.java
+++ src/main/java/br/edu/ifpr/demospring/demospring2/domain/Cargo.java
@@ -1,4 +1,22 @@
 package br.edu.ifpr.demospring.demospring2.domain;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "cargos")
 public class Cargo extends AbstractEntity <Long> {
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String nome;
+
+    @JoinColumn(name = "fk_id_departamento")
+    @ManyToOne
+    private Departamento departamento;
+
 }
