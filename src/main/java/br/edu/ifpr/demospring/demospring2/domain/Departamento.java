32d817123a99ab8dc82f95b5ec3dfe272088b305
IFPR <ifpr@jacarezinho.ifpr>
2024-07-31 20:24:10 -0300

    feat: lista de cargos criada em Departamento

--- src/main/java/br/edu/ifpr/demospring/demospring2/domain/Departamento.java
+++ src/main/java/br/edu/ifpr/demospring/demospring2/domain/Departamento.java
@@ -2,10 +2,13 @@ package br.edu.ifpr.demospring.demospring2.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,5 +19,8 @@ public class Departamento extends AbstractEntity <Long> {
 
     @Column(nullable = false, unique = true, length = 60)
     private String nome;
+
+    @OneToMany(mappedBy = "departamento")
+    List<Cargo> cargos;
 
 }
