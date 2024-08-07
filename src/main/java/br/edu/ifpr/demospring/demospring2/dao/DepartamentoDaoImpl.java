4b00b24683423ab4736437893e7c4bfcaff69f5d
IFPR <ifpr@jacarezinho.ifpr>
2024-08-07 20:56:59 -0300

    feat: criada classe DepartamentoDaoImpl

---
+++ src/main/java/br/edu/ifpr/demospring/demospring2/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,7 @@
+package br.edu.ifpr.demospring.demospring2.dao;
+
+import br.edu.ifpr.demospring.demospring2.domain.Departamento;
+
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao{
+
+}
